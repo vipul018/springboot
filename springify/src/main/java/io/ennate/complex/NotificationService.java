f71e986a20d9783d45a7bd08ae3a8a4da9d8d912
Vipul Sharma <vipulsharma018@gmail.com>
2019-12-01 21:13:25 -0500

    Autowired

--- springify/src/main/java/io/ennate/complex/NotificationService.java
+++ springify/src/main/java/io/ennate/complex/NotificationService.java
@@ -1,16 +1,20 @@
 package io.ennate.complex;
 
 import io.ennate.complex.email.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationService {
+    // This creates a property based DI
+    @Autowired
     private EmailService emailService;
 
-    NotificationService(EmailService emailService) {
-        System.out.println("Inside NotificationService class constructor");
-        this.emailService = emailService;
-    }
+//    @Autowired        // Not required for constructor based DI.
+//    NotificationService(EmailService emailService) {
+//        System.out.println("Inside NotificationService class constructor");
+//        this.emailService = emailService;
+//    }
 
     public void sendNotification(String to, String message) {
         System.out.println("Inside NotificationService class sendNotification method");
